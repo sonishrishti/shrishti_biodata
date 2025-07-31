004fa5a5cdb59b81b2b3b22b7404c5ecc6771fe1
Shrishti Soni <61708329+sonishrishti@users.noreply.github.com>
2025-07-31 14:39:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,23 +41,6 @@ function App() {
         </div>
       </div>
 
-      <div className = "parents">
-        <h1>Maa - Papa</h1>
-         <div className="parent-cards"> 
-            <div className="parent-card papa">
-              <h2>Maa</h2>
-              <p> Monika Sanjive Soni</p>
-              <p>House wife</p>
-            </div>
-            <div className="parent-card mumma">
-              <h2>Papa</h2>
-              <p>Sanjive P Soni</p>
-              <p>Business man - Automobile spareparts (dealing in heavy and commercial vehicles)</p>
-            </div>
-          </div>
-
-      </div>
-
       <div className="second-section">
         <div className="education">
           <h1>My life</h1>
